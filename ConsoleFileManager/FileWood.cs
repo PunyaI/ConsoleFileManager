34b81f64e64f74d5222bad39caf8172581b02808
Ilya <ronny0000@yandex.ru>
2021-08-21 18:14:06 +0300

    0.0.0.1

---
+++ ConsoleFileManager/FileWood.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFileManager
+{
+    class Tree
+    {
+        public static void Print(string dir)
+        {
+            Console.Clear();
+            Console.WriteLine("_______________________________________________________________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine(dir);
+            Console.WriteLine();
+            Tree.GetDirsTo2(dir);
+            Console.WriteLine("_______________________________________________________________________________________________________________________");
+            Console.WriteLine();
+            Tree.GetFilesFromDir(dir);
+            Console.WriteLine("_______________________________________________________________________________________________________________________");
+            Console.WriteLine();
+        }
+        public static void GetFilesFromDir(string dir)
+        {
+            string[] files = Directory.GetFiles(dir);
+            foreach(string file in files)
+            {
+                FileInfo file_info = new FileInfo(file);
+                Console.WriteLine("{0,35}     {1,10}      {2,10} КБ", file_info.Name, file_info.CreationTime, file_info.Length / 1024);
+            }
+        }
+        public static void GetDirsTo2(string start_path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(start_path);
+                foreach (string dir in dirs)
+                {
+                    DirectoryInfo dir_info = new DirectoryInfo(dir);
+                    Console.WriteLine("     ├─ " + dir_info.Name);
+                    string[] subdirs = Directory.GetDirectories(dir);
+                    foreach (string subdir in subdirs)
+                    {
+                        DirectoryInfo subdir_info = new DirectoryInfo(subdir);
+                        Console.WriteLine("     ├───── " + subdir_info.Name);
+                        string[] subdirs2 = Directory.GetDirectories(subdir);
+                        foreach (string subdir2 in subdirs2)
+                        {
+                            DirectoryInfo subdir_info2 = new DirectoryInfo(subdir2);
+                            Console.WriteLine("           " + "├───── " + subdir_info2.Name);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- ConsoleFileManager/Program.cs
+++ ConsoleFileManager/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleFileManager
 {
@@ -6,7 +11,33 @@ namespace ConsoleFileManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string startdir = "G:\\Новая папка\\";
+            Tree.Print(startdir);
+            do
+            {
+                Console.Write("Введите директорию ");
+                string dir = Console.ReadLine();
+                Tree.Print(dir);
+            } while (true);
+            
+            
+
+
+
+            // FileWood.GetFileWood("G:\\HomeWorkGB\\");
+
+
+            //string dirName = "G:\\";
+
+            //if (Directory.Exists(dirName))
+            //{
+            //    Console.WriteLine("Подкаталоги");
+            //    string[] dirs = Directory.GetDirectories(dirName);
+            //    foreach (string dir in dirs)
+            //    {
+            //        Console.WriteLine(dir);
+            //    }
+            //}    
         }
     }
 }
