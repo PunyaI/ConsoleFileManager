a27145e57714fe835c74aabea820549005796c46
Ilya <ronny0000@yandex.ru>
2021-08-22 14:00:02 +0300

    0.0.0.2

--- ConsoleFileManager/Program.cs
+++ ConsoleFileManager/Program.cs
@@ -11,16 +11,47 @@ namespace ConsoleFileManager
     {
         static void Main(string[] args)
         {
-            string startdir = "G:\\Новая папка\\";
-            Tree.Print(startdir);
+            //string startdir = "C:\\";
+            //Tree.Print(startdir);
+            //do
+            //{
+            //    Console.Write("Введите директорию ");
+            //    string dir = Console.ReadLine();
+            //    Tree.Print(dir);
+            //} while (true);
+            bool flag = false;
             do
             {
-                Console.Write("Введите директорию ");
-                string dir = Console.ReadLine();
-                Tree.Print(dir);
-            } while (true);
-            
-            
+                flag = Menu();
+            } while (!flag);
+
+            static bool Menu()
+            {
+                string value = Console.ReadLine();
+                string comand = Convert.ToString(value[0]) + Convert.ToString( value[1]);
+                if (comand!="ls")
+                {
+                    Console.WriteLine("Ошибка! Некорректная команда. ");
+                    return false;
+                }
+                Console.WriteLine(comand);
+                string userValue = value.Substring(3);
+                
+                string[] path = userValue.Split(",");
+                for (int i = 0; i < path.Length; i++)
+                {
+                    DirectoryInfo dir = new DirectoryInfo(path[i]);
+                    FileInfo file = new FileInfo(path[i]);
+                    if (!dir.Exists && !file.Exists)
+                    {
+                        Console.WriteLine("Ошибка! Некорректный путь.");
+                        return false;
+                    }
+                    path[i] = path[i].Trim();
+                    Console.WriteLine(path[i]);
+                }
+                return true;
+            }
 
 
 
