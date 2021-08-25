323774ede2974b0f3590464d9917c2fe4c3108a5
Ilya <ronny0000@yandex.ru>
2021-08-25 09:52:22 +0300

    0.8.0.0
    Рабочий менеджер, но требует отладки и возможно добавления нескольких нужных функций. Из важного ещё нет запоминания последней директории при запуске.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;                 ////тест меню
+            do
+            {
+                flag = FileComands.Menu();
+            } while (!flag);
+        }
+    }
+}
